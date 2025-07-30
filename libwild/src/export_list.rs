davidlattimore/wild

Implement more arguments for exporting symbols
For example used by deno:
https://github.com/denoland/deno/blob/45dfae18eec45c91146231aa05f4b249378691b5/ext/napi/generated_symbol_exports_list_linux.def

Fixes #955

libwild/src/export_list.rs
@@ -1,0 +1,142 @@
+use crate::error;
+use crate::error::Result;
+use crate::hash::PreHashed;
+use crate::input_data::ScriptData;
+use crate::linker_script::skip_comments_and_whitespace;
+use crate::symbol::UnversionedSymbolName;
+use crate::version_script::MatchRules;
+use crate::version_script::SymbolLookupNameWrapper;
+use crate::version_script::parse_matcher;
+use winnow::BStr;
+use winnow::Parser;
+
+#[derive(Default)]
+pub(crate) struct ExportList<'data>(MatchRules<'data>);
+
+impl<'data> ExportList<'data> {
+    pub(crate) fn parse(data: ScriptData<'data>) -> Result<Self> {
+        parse_export_list
+            .parse(BStr::new(data.raw))
+            .map_err(|err| error!("Failed to parse symbol export list:\n{err}"))
+    }
+
+    // Based on Version Script counterpart
+    pub(crate) fn contains(&self, name: &PreHashed<UnversionedSymbolName>) -> bool {
+        let mut lookup_name = SymbolLookupNameWrapper::from_name(name);
+
+        if self.0.general.matches_exact(&mut lookup_name, false)
+            || self.0.cxx.matches_exact(&mut lookup_name, true)
+        {
+            return true;
+        }
+
+        for &non_star in &[true, false] {
+            if self
+                .0
+                .general
+                .matches_glob(&mut lookup_name, non_star, false)
+                || self.0.cxx.matches_glob(&mut lookup_name, non_star, true)
+            {
+                return true;
+            }
+        }
+
+        if self.0.general.matches_all() || self.0.cxx.matches_all() {
+            return true;
+        }
+
+        false
+    }
+
+    pub(crate) fn add_symbol(&mut self, symbol: &'data str, without_colon: bool) -> Result<()> {
+        let matcher = parse_matcher(&mut BStr::new(symbol), without_colon)?;
+        self.0.push(matcher);
+        Ok(())
+    }
+}
+
+fn parse_export_list<'input>(input: &mut &'input BStr) -> winnow::Result<ExportList<'input>> {
+    let mut out = ExportList::default();
+
+    skip_comments_and_whitespace(input)?;
+
+    '{'.parse_next(input)?;
+
+    loop {
+        skip_comments_and_whitespace(input)?;
+
+        if input.starts_with(b"};") {
+            "};".parse_next(input)?;
+            skip_comments_and_whitespace(input)?;
+            break;
+        }
+
+        let matcher = parse_matcher(input, false)?;
+        out.0.push(matcher);
+    }
+
+    Ok(out)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::input_data::ScriptData;
+
+    #[test]
+    fn parse_inline() {
+        let data = ScriptData {
+            raw: b"{ f*; \"bar\"; extern \"C++\" { baz; qux; }; };",
+        };
+        let export_list = ExportList::parse(data).unwrap();
+        assert!(export_list.contains(&UnversionedSymbolName::prehashed(b"foo")));
+        assert!(export_list.contains(&UnversionedSymbolName::prehashed(b"bar")));
+        assert!(export_list.contains(&UnversionedSymbolName::prehashed(b"baz")));
+        assert!(export_list.contains(&UnversionedSymbolName::prehashed(b"qux")));
+        assert!(!export_list.contains(&UnversionedSymbolName::prehashed(b"not_exported")));
+    }
+
+    #[test]
+    fn parse_multiline_with_comments() {
+        let data = ScriptData {
+            raw: b"{
+                    # Single line comment
+                    foo;
+                    \"bar\"; # With a quote
+
+                    /*
+                    * And a C-style comment
+                    */
+                    baz*;
+
+                    extern \"C++\" {
+                        qux; # C++ symbol
+                    };
+                };",
+        };
+        let export_list = ExportList::parse(data).unwrap();
+        assert!(export_list.contains(&UnversionedSymbolName::prehashed(b"foo")));
+        assert!(export_list.contains(&UnversionedSymbolName::prehashed(b"bar")));
+        assert!(export_list.contains(&UnversionedSymbolName::prehashed(b"baz-test")));
+        assert!(export_list.contains(&UnversionedSymbolName::prehashed(b"qux")));
+        assert!(!export_list.contains(&UnversionedSymbolName::prehashed(b"not_exported")));
+    }
+
+    #[test]
+    fn externs() {
+        let data = ScriptData {
+            raw: b"{
+                    extern \"C\" {
+                        foo;
+                    };
+                    extern \"C++\" {
+                        bar;
+                    };
+                };",
+        };
+        let export_list = ExportList::parse(data).unwrap();
+        assert!(export_list.contains(&UnversionedSymbolName::prehashed(b"foo")));
+        assert!(export_list.contains(&UnversionedSymbolName::prehashed(b"bar")));
+        assert!(!export_list.contains(&UnversionedSymbolName::prehashed(b"not_exported")));
+    }
+}

libwild/src/version_script.rs
@@ -8,7 +8,7 @@ use crate::error;
 use crate::error::Result;
 use crate::hash::PassThroughHasher;
 use crate::hash::PreHashed;
-use crate::input_data::VersionScriptData;
+use crate::input_data::ScriptData;
 use crate::linker_script::skip_comments_and_whitespace;
 use crate::symbol::UnversionedSymbolName;
 use glob::Pattern;
@@ -23,9 +23,9 @@ use winnow::token::take_until;
 use winnow::token::take_while;
 
 #[derive(Debug, Default)]
-struct MatchRules<'data> {
-    general: BasicMatchRules<'data>,
-    cxx: BasicMatchRules<'data>,
+pub(crate) struct MatchRules<'data> {
+    pub(crate) general: BasicMatchRules<'data>,
+    pub(crate) cxx: BasicMatchRules<'data>,
 }
 
 #[derive(Debug, Default)]
@@ -48,7 +48,7 @@ pub(crate) struct VersionScript<'data> {
 }
 
 #[derive(Debug, PartialEq, Eq, Clone)]
-enum SymbolMatcher<'data> {
+pub(crate) enum SymbolMatcher<'data> {
     // Exact match.
     Exact(&'data [u8]),
     // A glob pattern with a '*' token.
@@ -60,7 +60,7 @@ enum SymbolMatcher<'data> {
 }
 
 #[derive(Debug, Default)]
-struct BasicMatchRules<'data> {
+pub(crate) struct BasicMatchRules<'data> {
     exact: HashSet<PreHashed<UnversionedSymbolName<'data>>, PassThroughHasher>,
     star_globs: Vec<Pattern>,
     nonstar_globs: Vec<Pattern>,
@@ -80,7 +80,11 @@ impl<'data> BasicMatchRules<'data> {
     }
 
     #[inline]
-    fn matches_exact(&self, lookup: &mut SymbolLookupNameWrapper, mangled: bool) -> bool {
+    pub(crate) fn matches_exact(
+        &self,
+        lookup: &mut SymbolLookupNameWrapper,
+        mangled: bool,
+    ) -> bool {
         // Early exit before we actually demangle the name.
         if self.exact.is_empty() {
             return false;
@@ -98,7 +102,7 @@ impl<'data> BasicMatchRules<'data> {
     }
 
     #[inline]
-    fn matches_glob(
+    pub(crate) fn matches_glob(
         &self,
         lookup: &mut SymbolLookupNameWrapper,
         non_star: bool,
@@ -122,6 +126,11 @@ impl<'data> BasicMatchRules<'data> {
 
         globs.any(|pattern| pattern.matches(name))
     }
+
+    #[inline]
+    pub(crate) fn matches_all(&self) -> bool {
+        self.matches_all
+    }
 }
 
 enum VersionRuleSection {
@@ -130,14 +139,14 @@ enum VersionRuleSection {
 }
 
 #[derive(Debug)]
-enum ParsedSymbolMatcher<'data> {
+pub(crate) enum ParsedSymbolMatcher<'data> {
     Single(SymbolMatcher<'data>),
     Multiple(Vec<SymbolMatcher<'data>>),
     CxxMatchers(Vec<SymbolMatcher<'data>>),
 }
 
 impl<'data> MatchRules<'data> {
-    fn push(&mut self, pattern: ParsedSymbolMatcher<'data>) {
+    pub(crate) fn push(&mut self, pattern: ParsedSymbolMatcher<'data>) {
         match pattern {
             ParsedSymbolMatcher::Single(single) => {
                 self.general.push(single);
@@ -156,14 +165,14 @@ impl<'data> MatchRules<'data> {
     }
 }
 
-struct SymbolLookupNameWrapper<'data> {
+pub(crate) struct SymbolLookupNameWrapper<'data> {
     name: &'data PreHashed<UnversionedSymbolName<'data>>,
     name_string: Option<&'data str>,
     demangled_name: Option<String>,
 }
 
 impl<'data> SymbolLookupNameWrapper<'data> {
-    fn from_name(name: &'data PreHashed<UnversionedSymbolName<'data>>) -> Self {
+    pub(crate) fn from_name(name: &'data PreHashed<UnversionedSymbolName<'data>>) -> Self {
         Self {
             name,
             name_string: None,
@@ -171,7 +180,7 @@ impl<'data> SymbolLookupNameWrapper<'data> {
         }
     }
 
-    fn get_name_string(&mut self) -> &'data str {
+    pub(crate) fn get_name_string(&mut self) -> &'data str {
         self.name_string.get_or_insert_with(|| {
             str::from_utf8(self.name.bytes()).unwrap_or_else(|_| {
                 panic!(
@@ -182,7 +191,7 @@ impl<'data> SymbolLookupNameWrapper<'data> {
         })
     }
 
-    fn get_demangled_name(&mut self) -> &String {
+    pub(crate) fn get_demangled_name(&mut self) -> &String {
         // Extract the name string before the closure to avoid double mutable borrow
         let name_string = self.get_name_string();
         self.demangled_name.get_or_insert_with(|| {
@@ -337,7 +346,7 @@ fn parse_version_script<'input>(input: &mut &'input BStr) -> winnow::Result<Vers
 
 impl<'data> VersionScript<'data> {
     #[tracing::instrument(skip_all, name = "Parse version script")]
-    pub(crate) fn parse(data: VersionScriptData<'data>) -> Result<VersionScript<'data>> {
+    pub(crate) fn parse(data: ScriptData<'data>) -> Result<VersionScript<'data>> {
         parse_version_script
             .parse(BStr::new(data.raw))
             .map_err(|err| error!("Failed to parse version script:\n{err}"))
@@ -407,7 +416,7 @@ fn parse_version_section<'data>(input: &mut &'data BStr) -> winnow::Result<Versi
             "local:".parse_next(input)?;
             section = Some(VersionRuleSection::Local);
         } else {
-            let matcher = parse_matcher(input)?;
+            let matcher = parse_matcher(input, false)?;
 
             match section {
                 Some(VersionRuleSection::Global) | None => {
@@ -423,7 +432,10 @@ fn parse_version_section<'data>(input: &mut &'data BStr) -> winnow::Result<Versi
     Ok(out)
 }
 
-fn parse_matcher<'data>(input: &mut &'data BStr) -> winnow::Result<ParsedSymbolMatcher<'data>> {
+pub(crate) fn parse_matcher<'data>(
+    input: &mut &'data BStr,
+    without_colon: bool, // e.g. symbol to export passed via CLI arg
+) -> winnow::Result<ParsedSymbolMatcher<'data>> {
     if input.starts_with(b"extern ") {
         let mut matchers = Vec::new();
         b"extern ".parse_next(input)?;
@@ -452,7 +464,7 @@ fn parse_matcher<'data>(input: &mut &'data BStr) -> winnow::Result<ParsedSymbolM
                 break;
             }
 
-            let matcher = parse_matcher(input)?;
+            let matcher = parse_matcher(input, without_colon)?;
             let ParsedSymbolMatcher::Single(matcher) = matcher else {
                 let unexpected_extern = if matches!(matcher, ParsedSymbolMatcher::CxxMatchers(_)) {
                     "C++"
@@ -474,7 +486,13 @@ fn parse_matcher<'data>(input: &mut &'data BStr) -> winnow::Result<ParsedSymbolM
         return Ok(ParsedSymbolMatcher::Multiple(matchers));
     }
 
-    let token = take_until(1.., b';').parse_next(input)?;
+    let token = if without_colon {
+        // TODO: Clippy bug
+        #[allow(clippy::needless_borrow)]
+        &input
+    } else {
+        take_until(1.., b';').parse_next(input)?
+    };
 
     skip_comments_and_whitespace(input)?;
 
@@ -562,7 +580,7 @@ mod tests {
 
     #[test]
     fn test_parse_simple_version_script() {
-        let data = VersionScriptData {
+        let data = ScriptData {
             raw: br#"
                     # Comment starting with a hash
                     {global:
@@ -606,7 +624,7 @@ mod tests {
 
     #[test]
     fn test_parse_version_script() {
-        let data = VersionScriptData {
+        let data = ScriptData {
             raw: br#"
                 VERS_1.1 {
                     global:
@@ -664,7 +682,7 @@ mod tests {
 
     #[test]
     fn single_line_version_script() {
-        let data = VersionScriptData {
+        let data = ScriptData {
             raw: br#"VERSION42 { global: *; };"#,
         };
         VersionScript::parse(data).unwrap();
@@ -672,7 +690,7 @@ mod tests {
 
     #[test]
     fn extern_cxx_version_script() {
-        let data = VersionScriptData {
+        let data = ScriptData {
             raw: br#"
                 "VERSION42 {
                     local:
@@ -736,7 +754,7 @@ mod tests {
 
     #[test]
     fn extern_c_version_script() {
-        let data = VersionScriptData {
+        let data = ScriptData {
             raw: br#"
                 "VERSION42 {
                     local:
@@ -766,7 +784,7 @@ mod tests {
     fn invalid_version_scripts() {
         #[track_caller]
         fn assert_invalid(src: &str) {
-            let data = VersionScriptData {
+            let data = ScriptData {
                 raw: src.as_bytes(),
             };
             assert!(VersionScript::parse(data).is_err());
@@ -787,7 +805,7 @@ mod tests {
 
     #[test]
     fn test_version_order() {
-        let data = VersionScriptData {
+        let data = ScriptData {
             raw: br#"
                 VERS_1.1 {
                     foo;

zanieb/sld

libwild/src/export_list.rs
@@ -1,0 +1,142 @@
+use crate::error;
+use crate::error::Result;
+use crate::hash::PreHashed;
+use crate::input_data::ScriptData;
+use crate::linker_script::skip_comments_and_whitespace;
+use crate::symbol::UnversionedSymbolName;
+use crate::version_script::MatchRules;
+use crate::version_script::SymbolLookupNameWrapper;
+use crate::version_script::parse_matcher;
+use winnow::BStr;
+use winnow::Parser;
+
+#[derive(Default)]
+pub(crate) struct ExportList<'data>(MatchRules<'data>);
+
+impl<'data> ExportList<'data> {
+    pub(crate) fn parse(data: ScriptData<'data>) -> Result<Self> {
+        parse_export_list
+            .parse(BStr::new(data.raw))
+            .map_err(|err| error!("Failed to parse symbol export list:\n{err}"))
+    }
+
+    // Based on Version Script counterpart
+    pub(crate) fn contains(&self, name: &PreHashed<UnversionedSymbolName>) -> bool {
+        let mut lookup_name = SymbolLookupNameWrapper::from_name(name);
+
+        if self.0.general.matches_exact(&mut lookup_name, false)
+            || self.0.cxx.matches_exact(&mut lookup_name, true)
+        {
+            return true;
+        }
+
+        for &non_star in &[true, false] {
+            if self
+                .0
+                .general
+                .matches_glob(&mut lookup_name, non_star, false)
+                || self.0.cxx.matches_glob(&mut lookup_name, non_star, true)
+            {
+                return true;
+            }
+        }
+
+        if self.0.general.matches_all() || self.0.cxx.matches_all() {
+            return true;
+        }
+
+        false
+    }
+
+    pub(crate) fn add_symbol(&mut self, symbol: &'data str, without_semicolon: bool) -> Result<()> {
+        let matcher = parse_matcher(&mut BStr::new(symbol), without_semicolon)?;
+        self.0.push(matcher);
+        Ok(())
+    }
+}
+
+fn parse_export_list<'input>(input: &mut &'input BStr) -> winnow::Result<ExportList<'input>> {
+    let mut out = ExportList::default();
+
+    skip_comments_and_whitespace(input)?;
+
+    '{'.parse_next(input)?;
+
+    loop {
+        skip_comments_and_whitespace(input)?;
+
+        if input.starts_with(b"};") {
+            "};".parse_next(input)?;
+            skip_comments_and_whitespace(input)?;
+            break;
+        }
+
+        let matcher = parse_matcher(input, false)?;
+        out.0.push(matcher);
+    }
+
+    Ok(out)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::input_data::ScriptData;
+
+    #[test]
+    fn parse_inline() {
+        let data = ScriptData {
+            raw: b"{ f*; \"bar\"; extern \"C++\" { baz; qux; }; };",
+        };
+        let export_list = ExportList::parse(data).unwrap();
+        assert!(export_list.contains(&UnversionedSymbolName::prehashed(b"foo")));
+        assert!(export_list.contains(&UnversionedSymbolName::prehashed(b"bar")));
+        assert!(export_list.contains(&UnversionedSymbolName::prehashed(b"baz")));
+        assert!(export_list.contains(&UnversionedSymbolName::prehashed(b"qux")));
+        assert!(!export_list.contains(&UnversionedSymbolName::prehashed(b"not_exported")));
+    }
+
+    #[test]
+    fn parse_multiline_with_comments() {
+        let data = ScriptData {
+            raw: b"{
+                    # Single line comment
+                    foo;
+                    \"bar\"; # With a quote
+
+                    /*
+                    * And a C-style comment
+                    */
+                    baz*;
+
+                    extern \"C++\" {
+                        qux; # C++ symbol
+                    };
+                };",
+        };
+        let export_list = ExportList::parse(data).unwrap();
+        assert!(export_list.contains(&UnversionedSymbolName::prehashed(b"foo")));
+        assert!(export_list.contains(&UnversionedSymbolName::prehashed(b"bar")));
+        assert!(export_list.contains(&UnversionedSymbolName::prehashed(b"baz-test")));
+        assert!(export_list.contains(&UnversionedSymbolName::prehashed(b"qux")));
+        assert!(!export_list.contains(&UnversionedSymbolName::prehashed(b"not_exported")));
+    }
+
+    #[test]
+    fn externs() {
+        let data = ScriptData {
+            raw: b"{
+                    extern \"C\" {
+                        foo;
+                    };
+                    extern \"C++\" {
+                        bar;
+                    };
+                };",
+        };
+        let export_list = ExportList::parse(data).unwrap();
+        assert!(export_list.contains(&UnversionedSymbolName::prehashed(b"foo")));
+        assert!(export_list.contains(&UnversionedSymbolName::prehashed(b"bar")));
+        assert!(!export_list.contains(&UnversionedSymbolName::prehashed(b"not_exported")));
+    }
+}

libwild/src/version_script.rs
@@ -8,7 +8,7 @@ use crate::error;
 use crate::error::Result;
 use crate::hash::PassThroughHasher;
 use crate::hash::PreHashed;
-use crate::input_data::VersionScriptData;
+use crate::input_data::ScriptData;
 use crate::linker_script::skip_comments_and_whitespace;
 use crate::symbol::UnversionedSymbolName;
 use glob::Pattern;
@@ -23,9 +23,9 @@ use winnow::token::take_until;
 use winnow::token::take_while;
 
 #[derive(Debug, Default)]
-struct MatchRules<'data> {
-    general: BasicMatchRules<'data>,
-    cxx: BasicMatchRules<'data>,
+pub(crate) struct MatchRules<'data> {
+    pub(crate) general: BasicMatchRules<'data>,
+    pub(crate) cxx: BasicMatchRules<'data>,
 }
 
 #[derive(Debug, Default)]
@@ -48,7 +48,7 @@ pub(crate) struct VersionScript<'data> {
 }
 
 #[derive(Debug, PartialEq, Eq, Clone)]
-enum SymbolMatcher<'data> {
+pub(crate) enum SymbolMatcher<'data> {
     // Exact match.
     Exact(&'data [u8]),
     // A glob pattern with a '*' token.
@@ -60,7 +60,7 @@ enum SymbolMatcher<'data> {
 }
 
 #[derive(Debug, Default)]
-struct BasicMatchRules<'data> {
+pub(crate) struct BasicMatchRules<'data> {
     exact: HashSet<PreHashed<UnversionedSymbolName<'data>>, PassThroughHasher>,
     star_globs: Vec<Pattern>,
     nonstar_globs: Vec<Pattern>,
@@ -80,7 +80,11 @@ impl<'data> BasicMatchRules<'data> {
     }
 
     #[inline]
-    fn matches_exact(&self, lookup: &mut SymbolLookupNameWrapper, mangled: bool) -> bool {
+    pub(crate) fn matches_exact(
+        &self,
+        lookup: &mut SymbolLookupNameWrapper,
+        mangled: bool,
+    ) -> bool {
         // Early exit before we actually demangle the name.
         if self.exact.is_empty() {
             return false;
@@ -98,7 +102,7 @@ impl<'data> BasicMatchRules<'data> {
     }
 
     #[inline]
-    fn matches_glob(
+    pub(crate) fn matches_glob(
         &self,
         lookup: &mut SymbolLookupNameWrapper,
         non_star: bool,
@@ -122,6 +126,11 @@ impl<'data> BasicMatchRules<'data> {
 
         globs.any(|pattern| pattern.matches(name))
     }
+
+    #[inline]
+    pub(crate) fn matches_all(&self) -> bool {
+        self.matches_all
+    }
 }
 
 enum VersionRuleSection {
@@ -130,14 +139,14 @@ enum VersionRuleSection {
 }
 
 #[derive(Debug)]
-enum ParsedSymbolMatcher<'data> {
+pub(crate) enum ParsedSymbolMatcher<'data> {
     Single(SymbolMatcher<'data>),
     Multiple(Vec<SymbolMatcher<'data>>),
     CxxMatchers(Vec<SymbolMatcher<'data>>),
 }
 
 impl<'data> MatchRules<'data> {
-    fn push(&mut self, pattern: ParsedSymbolMatcher<'data>) {
+    pub(crate) fn push(&mut self, pattern: ParsedSymbolMatcher<'data>) {
         match pattern {
             ParsedSymbolMatcher::Single(single) => {
                 self.general.push(single);
@@ -156,14 +165,14 @@ impl<'data> MatchRules<'data> {
     }
 }
 
-struct SymbolLookupNameWrapper<'data> {
+pub(crate) struct SymbolLookupNameWrapper<'data> {
     name: &'data PreHashed<UnversionedSymbolName<'data>>,
     name_string: Option<&'data str>,
     demangled_name: Option<String>,
 }
 
 impl<'data> SymbolLookupNameWrapper<'data> {
-    fn from_name(name: &'data PreHashed<UnversionedSymbolName<'data>>) -> Self {
+    pub(crate) fn from_name(name: &'data PreHashed<UnversionedSymbolName<'data>>) -> Self {
         Self {
             name,
             name_string: None,
@@ -171,7 +180,7 @@ impl<'data> SymbolLookupNameWrapper<'data> {
         }
     }
 
-    fn get_name_string(&mut self) -> &'data str {
+    pub(crate) fn get_name_string(&mut self) -> &'data str {
         self.name_string.get_or_insert_with(|| {
             str::from_utf8(self.name.bytes()).unwrap_or_else(|_| {
                 panic!(
@@ -182,7 +191,7 @@ impl<'data> SymbolLookupNameWrapper<'data> {
         })
     }
 
-    fn get_demangled_name(&mut self) -> &String {
+    pub(crate) fn get_demangled_name(&mut self) -> &String {
         // Extract the name string before the closure to avoid double mutable borrow
         let name_string = self.get_name_string();
         self.demangled_name.get_or_insert_with(|| {
@@ -337,7 +346,7 @@ fn parse_version_script<'input>(input: &mut &'input BStr) -> winnow::Result<Vers
 
 impl<'data> VersionScript<'data> {
     #[tracing::instrument(skip_all, name = "Parse version script")]
-    pub(crate) fn parse(data: VersionScriptData<'data>) -> Result<VersionScript<'data>> {
+    pub(crate) fn parse(data: ScriptData<'data>) -> Result<VersionScript<'data>> {
         parse_version_script
             .parse(BStr::new(data.raw))
             .map_err(|err| error!("Failed to parse version script:\n{err}"))
@@ -407,7 +416,7 @@ fn parse_version_section<'data>(input: &mut &'data BStr) -> winnow::Result<Versi
             "local:".parse_next(input)?;
             section = Some(VersionRuleSection::Local);
         } else {
-            let matcher = parse_matcher(input)?;
+            let matcher = parse_matcher(input, false)?;
 
             match section {
                 Some(VersionRuleSection::Global) | None => {
@@ -423,7 +432,10 @@ fn parse_version_section<'data>(input: &mut &'data BStr) -> winnow::Result<Versi
     Ok(out)
 }
 
-fn parse_matcher<'data>(input: &mut &'data BStr) -> winnow::Result<ParsedSymbolMatcher<'data>> {
+pub(crate) fn parse_matcher<'data>(
+    input: &mut &'data BStr,
+    without_semicolon: bool, // e.g. symbol to export passed via CLI arg
+) -> winnow::Result<ParsedSymbolMatcher<'data>> {
     if input.starts_with(b"extern ") {
         let mut matchers = Vec::new();
         b"extern ".parse_next(input)?;
@@ -452,7 +464,7 @@ fn parse_matcher<'data>(input: &mut &'data BStr) -> winnow::Result<ParsedSymbolM
                 break;
             }
 
-            let matcher = parse_matcher(input)?;
+            let matcher = parse_matcher(input, without_semicolon)?;
             let ParsedSymbolMatcher::Single(matcher) = matcher else {
                 let unexpected_extern = if matches!(matcher, ParsedSymbolMatcher::CxxMatchers(_)) {
                     "C++"
@@ -474,7 +486,13 @@ fn parse_matcher<'data>(input: &mut &'data BStr) -> winnow::Result<ParsedSymbolM
         return Ok(ParsedSymbolMatcher::Multiple(matchers));
     }
 
-    let token = take_until(1.., b';').parse_next(input)?;
+    let token = if without_semicolon {
+        // TODO: Clippy bug
+        #[allow(clippy::needless_borrow)]
+        &input
+    } else {
+        take_until(1.., b';').parse_next(input)?
+    };
 
     skip_comments_and_whitespace(input)?;
 
@@ -562,7 +580,7 @@ mod tests {
 
     #[test]
     fn test_parse_simple_version_script() {
-        let data = VersionScriptData {
+        let data = ScriptData {
             raw: br#"
                     # Comment starting with a hash
                     {global:
@@ -606,7 +624,7 @@ mod tests {
 
     #[test]
     fn test_parse_version_script() {
-        let data = VersionScriptData {
+        let data = ScriptData {
             raw: br#"
                 VERS_1.1 {
                     global:
@@ -664,7 +682,7 @@ mod tests {
 
     #[test]
     fn single_line_version_script() {
-        let data = VersionScriptData {
+        let data = ScriptData {
             raw: br#"VERSION42 { global: *; };"#,
         };
         VersionScript::parse(data).unwrap();
@@ -672,7 +690,7 @@ mod tests {
 
     #[test]
     fn extern_cxx_version_script() {
-        let data = VersionScriptData {
+        let data = ScriptData {
             raw: br#"
                 "VERSION42 {
                     local:
@@ -736,7 +754,7 @@ mod tests {
 
     #[test]
     fn extern_c_version_script() {
-        let data = VersionScriptData {
+        let data = ScriptData {
             raw: br#"
                 "VERSION42 {
                     local:
@@ -766,7 +784,7 @@ mod tests {
     fn invalid_version_scripts() {
         #[track_caller]
         fn assert_invalid(src: &str) {
-            let data = VersionScriptData {
+            let data = ScriptData {
                 raw: src.as_bytes(),
             };
             assert!(VersionScript::parse(data).is_err());
@@ -787,7 +805,7 @@ mod tests {
 
     #[test]
     fn test_version_order() {
-        let data = VersionScriptData {
+        let data = ScriptData {
             raw: br#"
                 VERS_1.1 {
                     foo;