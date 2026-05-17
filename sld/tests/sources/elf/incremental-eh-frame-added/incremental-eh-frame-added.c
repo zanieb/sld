4774d2a75054e2aaf06e4ba51345f7d1ae2e0496
Zanie Blue <contact@zanie.dev>
2026-05-16 23:28:26 -0700

    tests: Match CI formatting for eh-frame fixtures

--- sld/tests/sources/elf/incremental-eh-frame-added/incremental-eh-frame-added.c
+++ sld/tests/sources/elf/incremental-eh-frame-added/incremental-eh-frame-added.c
@@ -38,8 +38,7 @@ incremental_eh_frame_added_primary(void) {
 }
 
 #ifdef INCREMENTAL_EH_FRAME_ADDED
-__attribute__((section(".text.incremental_eh_frame_added"), noinline,
-               used)) int
+__attribute__((section(".text.incremental_eh_frame_added"), noinline, used)) int
 incremental_eh_frame_added_extra(void) {
   return 1;
 }
